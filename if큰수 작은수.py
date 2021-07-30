848dfa63c992f872385fc7ce81980f1282275ee3
zombiecode12 <86462106+zombiecode12@users.noreply.github.com>
2021-07-30 15:28:18 +0900

    2021.07.30

---
+++ if큰수 작은수.py
@@ -0,0 +1,9 @@
+num1 = int(input("수를 입력해 주세요"))
+num2 = int(input("수를 입력해 주세요"))
+
+if (num1<num2):
+    print("큰수:%d"%num2)
+if (num1>num2):
+    print("큰수:%d"%num1)
+if (num1==num2):
+    print("수가 같습니다.")
