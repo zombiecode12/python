caa5a0c13dfac66c9b594d3c28a97da0487d10d3
zombiecode12 <86462106+zombiecode12@users.noreply.github.com>
2021-07-30 15:45:44 +0900

    2021.07.30

---
+++ if-else.py
@@ -0,0 +1,15 @@
+num = int(input("점수를 입력해 주세요"))
+if (num>=80):
+    print("합격입니다.")
+else:
+    print("탈락입니다.")
+"""
+if-else
+if (조건):
+   실행문
+else :
+    실행문
+else 는 조건이 없다.
+if에 해당되지 않는 모든것이 else
+"""
+
