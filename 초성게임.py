c042ae6351d14db459fce9359c442e6a66c9619a
zombiecode12 <86462106+zombiecode12@users.noreply.github.com>
2021-09-17 15:50:12 +0900

    2021.09.17

---
+++ 초성게임.py
@@ -0,0 +1,14 @@
+print("못맞추는 초성게임")
+while True :
+    startAnswer=input("Do you want to start game now? (yes/no)")
+    if startAnswer == "yes":
+        print("game start")
+        break
+    elif startAnswer == "no":
+        print("good bye")
+        exit()
+    else :
+        print("erro")
+print("first problem")
+print("subject:전자제품")
+print("ㅈㅈㄹㅇㅈ")
