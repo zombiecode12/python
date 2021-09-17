c85cea2350bb420576e538fef51680500abc32c9
zombiecode12 <86462106+zombiecode12@users.noreply.github.com>
2021-09-17 15:14:29 +0900

    2021.09.17

---
+++ while 무한반복.py
@@ -0,0 +1,13 @@
+"""
+while 무한반복.py
+while의 조건이 계속 참이면 계속 무한반복
+조건 :True / Fales
+True : 계속 무한 반복
+False : 계속 무한 거짓 > 한번도 실행되지 않는다.
+break : while에서 탈출하는 명령어
+"""
+while False :
+    print("while 무한거짓")
+while True :
+    print("while 무한반복")
+    break
